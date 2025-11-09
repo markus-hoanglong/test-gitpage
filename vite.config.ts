c3607b62a605f4cfeaf186dd14a84ddec05ce628
markus.hoang <markus.hoanglong@gmail.com>
2025-11-09 22:59:49 +0700

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/markus-hoanglong/test-gitpage'
+  base: 'https://markus-hoanglong.github.io/test-gitpage'
 })
